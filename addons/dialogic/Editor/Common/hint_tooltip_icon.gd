ca76602f992fabe9bff829464530dd8b1c65e5d0
Travh98 <travishunter33@gmail.com>
2024-03-03 06:39:15 -0500

    Removed Dialogic, made it a submodule

--- addons/dialogic/Editor/Common/hint_tooltip_icon.gd
+++
@@ -1,9 +0,0 @@
-@tool
-extends TextureRect
-
-@export_multiline var hint_text = ""
-
-func _ready():
-	texture = get_theme_icon("NodeInfo", "EditorIcons")
-	modulate = get_theme_color("readonly_color", "Editor")
-	tooltip_text = hint_text
--- addons/dialogic/Modules/StyleEditor/index.gd
+++
@@ -1,4 +0,0 @@
-extends DialogicIndexer
-
-func _get_editors() -> Array:
-	return [this_folder.path_join('style_editor.tscn')]
